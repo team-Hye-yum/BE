0f579a4b5d33d175dd9862b8643e461238be33f6
uiseok <dkdlelxoa9@gmail.com>
2026-07-16 17:27:01 +0900

    ✨ Feature: 초기 세팅

---
+++ src/main/java/site/dataon/hyeyum/HyeyumApplication.java
@@ -0,0 +1,13 @@
+package site.dataon.hyeyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyeyumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/dataon/hyeyum/HyeyumApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dataon.hyeyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyeyumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
